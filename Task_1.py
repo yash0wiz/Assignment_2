d8f50a9aa185edb2d136576a82b8bbe6263fc294
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 19:04:21 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,11 @@
+ 
+Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+
+
+
+
 
