06e24407f380036096b6df90955fbeac114908db
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 19:35:02 +0530

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,8 +1,14 @@
 
-Task 2: Sum of Integers from 1 to 50 Using a Loop
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+#Problem Statement: Write a Python program that:
+#1.   Uses a for loop to iterate over numbers from 1 to 50.
+#2.   Calculates the sum of all integers in this range.
+#3.   Displays the final sum.
  
-Problem Statement: Write a Python program that:
-1.   Uses a for loop to iterate over numbers from 1 to 50.
-2.   Calculates the sum of all integers in this range.
-3.   Displays the final sum.
- 
+i=1
+num1=0
+while i<=50:
+    num1=num1+i
+    i=i+1
+
+print("The sum of numbers from 1 to 50 is: ", num1)
