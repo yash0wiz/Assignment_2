6e4427b004567f00c73881fda08a6edc2948c602
yash0wiz <141996224+yash0wiz@users.noreply.github.com>
2025-05-30 19:30:33 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,11 +1,13 @@
  
-Task 1: Check if a Number is Even or Odd
-Problem Statement:  Write a Python program that:
-1. 	Takes an integer input from the user.
-2. 	Checks whether the number is even or odd using an if-else statement.
-3. 	Displays the result accordingly.
+#Task 1: Check if a Number is Even or Odd
+#Problem Statement:  Write a Python program that:
+#1. 	Takes an integer input from the user.
+#2. 	Checks whether the number is even or odd using an if-else statement.
+#3. 	Displays the result accordingly.
 
+num1=int(input("Enter the first number: "))
 
-
-
-
+if (num1%2)==1:
+    print(num1," is an odd number")
+else:
+    print(num1," is an even number")
